5a379600903d82fab9fd992700cb7521e82a9660
hortemo <henrikhortemo@icloud.com>
2025-11-21 14:22:07 +0100

    forceRenewalOfSubscription

--- example/ios/ExpoStoreKitUITests/ExpoStoreKitUITests.swift
+++ example/ios/ExpoStoreKitUITests/ExpoStoreKitUITests.swift
@@ -2,9 +2,15 @@ import XCTest
 import StoreKitTest
 
 final class ExpoStoreKitUITests: XCTestCase {
+  private var session: SKTestSession!
+  
   override func setUpWithError() throws {
-    let session = try SKTestSession(configurationFileNamed: "ExpoStoreKitConfiguration")
+    session = try SKTestSession(configurationFileNamed: "ExpoStoreKitConfiguration")
     session.clearTransactions()
+  }
+  
+  override func tearDownWithError() throws {
+    session = nil
   }
   
   @MainActor
@@ -15,6 +21,15 @@ final class ExpoStoreKitUITests: XCTestCase {
     let runTestsButton = app.buttons["run-tests"]
     XCTAssertTrue(runTestsButton.waitForExistence(timeout: 20))
     runTestsButton.tap()
+    
+    DispatchQueue.main.asyncAfter(deadline: .now() + 10) { [weak self] in
+      guard let self else { return }
+      do {
+        try self.session.forceRenewalOfSubscription(productIdentifier: "expo.storekit.subscription1")
+      } catch {
+        XCTFail("Failed to force subscription renewal: \(error)")
+      }
+    }
     
     let statusLabel = app.staticTexts["test-status"]
     XCTAssertEqual(statusLabel.wait(for: \.label, toEqual: "Status: success", timeout: 60), true)
